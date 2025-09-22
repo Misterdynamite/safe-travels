53931f48f07752bdcdda344c6a03fef8443103c6
Misterdynamite <51722780+Misterdynamite@users.noreply.github.com>
2025-09-22 12:05:40 +1200

    Create Program.cs

---
+++ API-Tester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using AucklandTransportAPI;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var stopsCalls = new StopsCalls();
+        var results = await stopsCalls.GetStopsByName("Torbay Shops");
+        StopData firstResult = results[0];
+        Console.WriteLine($"Found {results.Count} stops matching 'Torbay'. First stop ID: {firstResult.attributes.stopId}, Name: {firstResult.attributes.stopName}");
+        var stopTripCalls = new StopTripsCalls();
+        var tripResults = await stopTripCalls.GetTripsByStopID(firstResult.attributes.stopId);
+        Console.WriteLine($"Found {tripResults.Count} trips for stop {firstResult.attributes.stopName}");
+    }
+}
